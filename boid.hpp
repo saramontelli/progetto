9d5b924700cc20cc4019d7084a2acac3bcd22328
sara-montelli <sara.montelli@studio.unibo.it>
2026-01-16 16:14:43 +0100

    aggiunto

---
+++ boid.hpp
@@ -0,0 +1,36 @@
+#ifndef BOID_HPP
+#define BOID_HPP
+#include <vector>
+
+#include "vector.hpp"
+
+namespace math {
+
+class Boid {
+ private:
+  Vector vel_;
+  Vector pos_;
+
+ public:
+  Boid();
+  Boid(Vector position, Vector velocity);
+
+  Vector get_pos() const;
+  Vector get_vel() const;
+
+  // forza che allontana dai boids vicini
+  Vector separation(const std::vector<Boid>& boids, float s, float d_s) const;
+  // il boids si deve allineare con quelli vicini
+  Vector alignment(const std::vector<Boid>& boids, float a) const;
+  // boid si muove verso il baricentro dei boids vicini
+  Vector cohesion(const std::vector<Boid>& boids, float c) const;
+  // boids nelle vicinanze
+  std::vector<Boid> get_neighbors(const std::vector<Boid>& all_boids,
+                                  float d) const;
+
+  void change_vel(const Vector&);
+  void change_pos(const Vector&);
+};
+}  // namespace math
+
+#endif
